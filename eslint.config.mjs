febc44e9183b6bd96073290e35b4bc31acb6a4a6
darcrand <darcrandex@gmail.com>
2025-04-29 18:11:25 +0800

    🎈 perf(all): lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,3 @@
-// import { FlatCompat } from '@eslint/eslintrc'
-// import { dirname } from 'path'
-// import { fileURLToPath } from 'url'
-
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = dirname(__filename)
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// })
-
-// const eslintConfig = [
-//   ...compat.extends('next/core-web-vitals', 'next/typescript'),
-
-//   ...compat.config({
-//     rules: {
-//       '@typescript-eslint/no-use-before-define': 'off',
-//       '@typescript-eslint/no-unused-vars': 'warn',
-//       '@typescript-eslint/no-explicit-any': 'off',
-//       'react/react-in-jsx-scope': 'off',
-//       'react/prop-types': 'off',
-//     },
-//   }),
-// ]
-
-// export default eslintConfig
-
 import { FlatCompat } from '@eslint/eslintrc'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
@@ -36,6 +9,18 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript')]
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+
+  ...compat.config({
+    rules: {
+      '@typescript-eslint/no-use-before-define': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+    },
+  }),
+]
 
 export default eslintConfig
